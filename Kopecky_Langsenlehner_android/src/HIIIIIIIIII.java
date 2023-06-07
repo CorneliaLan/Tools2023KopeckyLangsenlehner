807d827d03f6893b98326c9e9f6ce9c2193919fd
CorneliaLan <cornelialangsenlehner@gmail.com>
2023-06-07 16:56:23 +0200

    Monty was invited to play a game of life and death

--- Kopecky_Langsenlehner_android/src/HIIIIIIIIII.java
+++ Kopecky_Langsenlehner_android/src/HIIIIIIIIII.java
@@ -1,7 +1,9 @@
 
 public class HIIIIIIIIII {
 <<<<<<< HEAD
-	public void thisIsANewMethod() { }
+	public void thisIsANewMethod() { 
+	System.out.println("Once upon a time...");//keep searching for the eastereggs :)
+}
 =======
 	
 	public static void main(String[] args) {
