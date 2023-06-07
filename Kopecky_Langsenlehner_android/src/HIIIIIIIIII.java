147133b35b76d7d5a0e5f29f910fdc33c1878f38
CorneliaLan <cornelialangsenlehner@gmail.com>
2023-06-07 17:53:26 +0200

    his 5th star was taken from him

--- Kopecky_Langsenlehner_android/src/HIIIIIIIIII.java
+++ Kopecky_Langsenlehner_android/src/HIIIIIIIIII.java
@@ -3,13 +3,15 @@ public class HIIIIIIIIII {
 <<<<<<< HEAD
 	public void thisIsANewMethod() { 
 	System.out.println("Once upon a time...");//keep searching for the eastereggs :)
+	System.out.println("At the park-island he collected his third star and some coins too");
 }
 =======
 	
 	public static void main(String[] args) {
 		
 		System.out.print("HIIIIIIIIII");
-	
+		System.out.println("At the watermelon-island he couldn't reach the star but");
+		System.out.println("At the left island he collected his first star with a golden pipe that helps to get to the next Star");
 	}
 	
 >>>>>>> 50d68166f3cd0688d783fe634f55a6fe4e837ecc
